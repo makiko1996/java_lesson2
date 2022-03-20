6d0b4af7eda5c3b377c88aad48b8e2eca480ea5a
makiko1996 <t_makiko_1996@yahoo.co.jp>
2022-03-20 21:58:50 +0900

    [add]prigate_java

---
+++ Main.java
@@ -0,0 +1,14 @@
+package jp.practice.javastudy;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Bicycle bicycle = new Bicycle("ビアンキ", "緑");
+	    
+	    bicycle.bicyclePrint();
+	    bicycle.run(10);
+
+	}
+
+}
