705b99196d4153b16c9a899fc69206c2d4811edb
makiko1996 <t_makiko_1996@yahoo.co.jp>
2022-03-21 21:11:47 +0900

    [add]abc085b

---
+++ abc085b.java
@@ -0,0 +1,22 @@
+package jp.practice.javastudy;
+
+import java.util.*;
+
+public class abc085b {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		Set<Integer> d = new HashSet<Integer>();
+		
+		for (int i = 1; i <= n ;i++) {
+			d.add(scan.nextInt());
+		}
+		
+		System.out.println(d.size());
+
+	}
+
+}
