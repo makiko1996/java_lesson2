5f829f8efc891d6d32653a4db061a03a35c7bcda
makiko1996 <t_makiko_1996@yahoo.co.jp>
2022-03-22 16:26:08 +0900

    [fix]abc006b

--- abc006a.java
+++ abc006a.java
@@ -1,9 +1,20 @@
 package jp.practice.javastudy;
 
+import java.util.Scanner;
+
 public class abc006a {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		Scanner scan = new Scanner(System.in);
+
+		int n = scan.nextInt();
+
+		if (n % 3 == 0) {
+			System.out.println("YES");
+		} else {
+			System.out.println("NO");
+		}
 
 	}
 
--- abc006b.java
+++ abc006b.java
@@ -1,23 +1,25 @@
 package jp.practice.javastudy;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class abc006b {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		Scanner scan = new Scanner(System.in);
-		int N = scan.nextInt();
-		int[] n = new int[N + 3];
-		n[3] = 1;
-
-		for (int i = 4; i <= N; i++) {
-			n[i] = n[i - 1] + n[i - 2] + n[i - 3];
-			n[i] %= 10007;
+		int n = scan.nextInt();
+		int[] list = new int[n];
+		if (n > 2) {
+			list[2] = 1;
 		}
+		int ans = 0;
 
-		System.out.println(n[N]);
-
+		for (int i = 3; i < n; i++) {
+			list[i] = (list[i - 1] + list[i - 2] + list[i - 3]) % 10007;
+		}
+		
+		System.out.println(list[n-1]);
+		
 	}
 
 }
