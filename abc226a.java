05a83ee6c12f34f9b645bdcc051e62f2b6282db6
makiko1996 <t_makiko_1996@yahoo.co.jp>
2022-03-23 17:03:09 +0900

    [add]abc226a

---
+++ abc226a.java
@@ -0,0 +1,17 @@
+package jp.practice.javastudy;
+
+import java.util.Scanner;
+
+public class abc226a {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner scan = new Scanner(System.in);
+		
+		double x = scan.nextDouble();
+		
+		System.out.println(Math.round(x));
+
+	}
+
+}
