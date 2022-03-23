87358d571043d7e7629987f686612ddaa4e788c0
makiko1996 <t_makiko_1996@yahoo.co.jp>
2022-03-23 19:29:00 +0900

    [add]abc226b

---
+++ abc226b.java
@@ -0,0 +1,24 @@
+package jp.practice.javastudy;
+
+import java.util.*;
+
+public class abc226b {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		Set<String> set = new HashSet<String>();
+		
+		for (int i = 0; i < N+1; i++) {
+			System.out.println("------------");
+			set.add(scan.nextLine());
+			System.out.println(i);
+		}
+		
+		System.out.println(set.size() - 1);
+
+		
+		
+	}
+
+}
