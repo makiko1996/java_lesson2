d8c493ad6f867dcb3b1881647e470d1b360f2f0c
makiko1996 <t_makiko_1996@yahoo.co.jp>
2022-03-26 19:22:10 +0900

    [add]abc244b

--- abc244a.java
+++ abc244a.java
@@ -6,14 +6,13 @@ public class abc244a {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		
+
 		Scanner scan = new Scanner(System.in);
 		int N = scan.nextInt();
 		String S = scan.next();
-		
+
 		String ans = S.substring(N - 1);
 		System.out.println(ans);
-		
 
 	}
 
---
+++ abc244b.java
@@ -0,0 +1,57 @@
+package jp.practice.javastudy;
+
+import java.util.Scanner;
+
+public class abc244b {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		String t = scan.next();
+		String direction = "E";
+		int x = 0;
+		int y = 0;
+		
+		for (int i = 0; i < n; i++) {
+			String T = t.substring(i, i + 1);
+			if (T.equals("S")) {
+				switch(direction) {
+					case "N":
+						y++;
+						break;
+					case "E":
+						x++;
+						break;
+					case "S":
+						y--;
+						break;
+					case "W":
+						x--;
+						break;
+				}
+			} else if (T.equals("R")){
+				switch(direction) {
+					case "N":
+						direction = "E";
+						break;
+					case "E":
+						direction = "S";
+						break;
+					case "S":
+						direction = "W";
+						break;
+					case "W":
+						direction = "N";
+						break;
+				}
+				
+			}
+		}
+		
+		System.out.println("(" + x + "," + y +")");
+
+	}
+
+}
