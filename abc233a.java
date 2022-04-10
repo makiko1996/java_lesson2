bfd192675e0661fc2b3c8015a59be971e9697cc7
makiko1996 <t_makiko_1996@yahoo.co.jp>
2022-04-10 19:11:25 +0900

    [fix]abc233a

--- abc233a.java
+++ abc233a.java
@@ -5,15 +5,18 @@ import java.util.Scanner;
 public class abc233a {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
+
 		double x = scan.nextInt();
 		double y = scan.nextInt();
+
+		double ans = Math.ceil((y - x) / 10);
 		
-		double ans = Math.ceil((x - y) / 10);
-		
-		System.out.println((int)ans);
-		
-		
+		if (ans > 0) {
+			System.out.println((int) ans);
+		} else {
+			System.out.println(0);
+		}
+
 	}
 
 }
