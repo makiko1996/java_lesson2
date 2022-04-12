55e0fd65321678f0a2ce180f7d07c16f34aa35d1
makiko1996 <t_makiko_1996@yahoo.co.jp>
2022-04-12 18:17:08 +0900

    [add]abc232b

---
+++ abc232b.java
@@ -0,0 +1,27 @@
+package jp.practice.javastudy;
+
+import java.util.Scanner;
+
+public class abc232b {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String s = scan.next();
+		String t = scan.next();
+		String ans = "Yes";
+		
+		int k = (t.charAt(0) - s.charAt(0));
+//		int k = (t.charAt(0) - s.charAt(0) + 26) % 26;
+		int n = s.length();
+		
+		for (int i = 1; i < n; i++) {
+			int tmp = (t.charAt(i) - s.charAt(i));
+//			int tmp = (t.charAt(i) - s.charAt(i) + 26) % 26;
+			if (tmp != k) {
+				ans = "No";
+				break;
+			}
+		}
+		System.out.println(ans);
+	}
+
+}
