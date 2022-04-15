1ba3fb94f02d7d36763c64e251fdea6a39b22309
makiko1996 <t_makiko_1996@yahoo.co.jp>
2022-04-15 18:31:30 +0900

    [add]abc230a

---
+++ abc230a.java
@@ -0,0 +1,16 @@
+package jp.practice.javastudy;
+
+import java.util.Scanner;
+
+public class abc230a {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		
+		if(n >= 42)n++;
+		
+		
+		System.out.println("AGC" + String.format("%03d", n));
+	}
+
+}
